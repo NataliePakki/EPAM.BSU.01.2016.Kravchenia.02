b3982352661f0f057eb33a7862620d56a7d099d4
Natalie <fecit@inbox.ru>
2016-03-12 02:29:35 +0300

    add Task1 and Task1.Tests

---
+++ Task1.Tests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+
+    [TestFixture]
+    public class CalculatorTests {
+        [Test]
+        [TestCase(4, 2, Result = 2)]
+        [TestCase(27, 3, Result = 3)]
+        public double RootNewtonMethodTest(int number, int n) {
+            return Calculator.RootNewtonMethod(number, n, 0);
+        }
+
+        [Test]
+        [ExpectedException(typeof (ArgumentException))]
+        [TestCase(-4, 2)]
+        [TestCase(27, 0)]
+        [TestCase(27, -3)]
+        public void RootNewtonMethodTest_Exception(int number, int n) {
+            Assert.AreEqual(Calculator.RootNewtonMethod(number, n, 0), 0);
+        }
+
+        [Test]
+        [TestCase(13, 4, 1E-10)]
+        [TestCase(3, 14, 1E-16)]
+        public void RootNewtonMethodTest_Eps(int number, int n, double eps) {
+            double resPow = Math.Pow(number, (double) 1/n);
+            double resNewton = Calculator.RootNewtonMethod(number, n, eps);
+
+            Assert.IsTrue(Math.Abs(resPow - resNewton) <= eps);
+        }
+    }
+}
---
+++ Task1/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    public static class Calculator
+    {
+        public static double RootNewtonMethod(int number, int n, double eps)
+        {
+            if (number < 0)
+                throw new ArgumentException("Number must not be negative.", "number");
+            if (n <= 0)
+                throw new ArgumentException("n must be positive", "n");
+            double xk1 = (double)number / n;
+            double xk;
+            do
+            {
+                xk = xk1;
+                xk1 = ((n - 1) * xk + number / Math.Pow(xk, n - 1)) / n;
+            } while (Math.Abs(xk - xk1) > eps);
+            return xk1;
+        }
+    }
+}
