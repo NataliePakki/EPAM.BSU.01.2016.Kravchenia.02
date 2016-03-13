194ae9efca50922f0b8252a1069379b06d46137f
Natalie <fecit@inbox.ru>
2016-03-13 17:38:52 +0300

    add Task4 and Task4.Tests

---
+++ Task4.Tests/CustomerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4.Tests {
+    public class CustomerTests {
+        private Customer cust = new Customer("John", "+375-17-4341283", 10000);
+        private class TestFormatProvider : IFormatProvider, ICustomFormatter
+        {
+            public string Format(string format, object arg, IFormatProvider formatProvider){
+                Customer t = arg as Customer;
+                if (t == null) { return arg.ToString(); }
+                return "Customer record: " + t.ContactPhone + ", " + t.Name + ", " + t.Recenue;
+            }
+
+            public object GetFormat(Type formatType){
+                return (formatType == typeof(Customer)) ? this : null;
+            }
+        }
+
+        [Test]
+        [TestCase(Result = "Customer record: John, +375-17-4341283, 10000")]
+        public string CustomerToStringTest() {
+            return cust.ToString();
+        }
+
+        [Test]
+        [TestCase(Result = "Customer record: +375-17-4341283, John, 10000")]
+        public string CustomerToStringTest_AnotherFormat() {
+            return cust.ToString(new TestFormatProvider());
+        }
+
+        [Test]
+        [TestCase(Result = "Customer record: John, +375-17-4341283")]
+        public string CustomerToStringTest_formatNamePhone() {
+            return cust.ToString("name phone");
+        }
+        [Test]
+        [ExpectedException(typeof(FormatException))]
+        [TestCase(Result = "Customer record: John, 10000")]
+        public string CustomerToStringTest_Exception() {
+            return cust.ToString("name recenue");
+        }
+    }
+}
---
+++ Task4/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task4 {
+    public class Customer:IFormattable {
+        public string Name {get; set;}
+        public string ContactPhone {get; set;}
+        public decimal Recenue { get; set; }
+
+        public Customer(string name, string phone, decimal rec) {
+            Name = name;
+            ContactPhone = phone;
+            Recenue = rec;
+        }
+        public override string ToString() {
+            return ToString("g");
+        }
+        
+        public string ToString(string format, IFormatProvider formatProvider) {
+            if (String.IsNullOrEmpty(format)) format = "g"; 
+             if (formatProvider != null) {
+                 ToString(formatProvider);
+             }
+            return ToString(format);
+
+        }
+
+        public string ToString(IFormatProvider formatProvider) {
+            if (formatProvider != null) {
+                var fmt = formatProvider.GetFormat(GetType()) as ICustomFormatter;
+                if (fmt != null) { return fmt.Format(null, this, formatProvider); }
+            }
+            return " ";
+        }
+        public string ToString(string format) {
+                switch (format.ToLower()) {
+                    case "g":
+                    case "name phone recenue":
+                        return String.Format("Customer record: {0}, {1}, {2}", Name, ContactPhone, Recenue);
+                    case "name":
+                        return String.Format("Customer record: {0}", Name);
+                    case "name phone":
+                        return String.Format("Customer record: {0}, {1}", Name, ContactPhone);
+                    case "phone":
+                        return String.Format("Customer record: {0}", ContactPhone);
+                    case "recenue":
+                        return String.Format("Customer record: {0}", Recenue);
+                    default:
+                        throw new FormatException(String.Format("The '{0}' format string is not supported.", format));
+            }
+        }
+    }
+}
