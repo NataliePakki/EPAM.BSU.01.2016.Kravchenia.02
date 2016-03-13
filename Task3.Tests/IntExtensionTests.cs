d4e473bf81b0a996fb42304037f02baa83db3cbd
Natalie <fecit@inbox.ru>
2016-03-13 22:41:07 +0300

    add Task3 and Task3.Tests

---
+++ Task3.Tests/IntExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+namespace Task3.Tests {
+    [TestFixture]
+    public class IntExtensionTests {
+        [Test]
+        [TestCase(0, Result = "0")]
+        [TestCase(4, Result = "4")]
+        [TestCase(27, Result = "1B")]
+        public string ToHexStringTest(int number) {
+            return number.ToHexString();
+        }
+
+        [TestCase(-24, Result = "-18")]
+        [TestCase(-240, Result = "-F0")]
+        public string ToHexStringTest_Negative(int number) {
+            return number.ToHexString();
+        }
+    }
+}
---
+++ Task3/IntExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task3 {
+    public static class IntExtension {
+
+        public static string ToHexString(this int number) {
+            bool isNegative = false;
+            const string digits = "0123456789ABCDEF";
+            if (number == 0) return "0";
+            if (number < 0) {
+                isNegative = true;
+                number = Math.Abs(number);
+            }
+            string hex = "";
+            while (number > 0)
+            {
+                int digit = number % 16; 
+                hex = digits[digit] + hex; 
+                number = number / 16;
+            }
+            if (isNegative)
+                hex = "-" + hex;
+            return hex;
+        }
+    }
+}
