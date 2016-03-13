facfa87c8c22fa315a6b461a4c4aece51d04d31a
Natalie <fecit@inbox.ru>
2016-03-14 01:24:39 +0300

    correct mistake, add time measurement

--- Task2.Tests/GreatestCommonDivisorTests.cs
+++ Task2.Tests/GreatestCommonDivisorTests.cs
@@ -4,40 +4,34 @@ using NUnit.Framework;
 namespace Task2.Tests
 {
     [TestFixture]
-    public class GreatestCommonDivisorTests
-    {
+    public class GreatestCommonDivisorTests {
+        private TimeSpan _time;
         [Test]
         [TestCase(27, 9,Result = 9)]
         [TestCase(27,0, Result = 27)]
-        [TestCase(3,15, Result = 3)]
+        [TestCase(45,60, Result = 15)]
         [TestCase(-3,15, Result = 3)]
         [TestCase(3, 4, Result = 1)]
         public int EuclideanMethodTest(int a, int b) {
-            return GreatestCommonDivisor.EuclideanMethod(a, b);
+            return GreatestCommonDivisor.EuclideanMethod(out _time, a, b);
         }
-
+        [Test]
+        [TestCase(15, 45, 3, 15, Result = 3)]
+        [TestCase(5, 45, 5, 1, 5, Result = 1)]
+        public int EuclideanMethodTest_params(params int[] data){
+            return GreatestCommonDivisor.EuclideanMethod(out _time, data);
+        }
         [Test]
         [TestCase(0, 0)]
         [ExpectedException(typeof(ArgumentException))]
-        public void EuclideanMethodTest_Exception(int a, int b)
-        {
-            Assert.AreEqual(GreatestCommonDivisor.EuclideanMethod(a, b),0);
-        }
-
-
-        [Test]
-        [TestCase(3, 15, 45, Result = 3)]
-        [TestCase(45, 15, 45, Result = 15)]
-        public int EuclideanMethodTest_3arg(int a, int b, int c)
-        {
-            return GreatestCommonDivisor.EuclideanMethod(a, b, c);
+        public void EuclideanMethodTest_ZeroException(int a, int b) {
+            Assert.AreEqual(GreatestCommonDivisor.EuclideanMethod(out _time, a, b), 0);
         }
         [Test]
-        [TestCase(15,45,3,15, Result = 3)]
-        [TestCase(5, 45, 5, 1, 5 , Result = 1)]
-        public int EuclideanMethodTest_params(params int[] data)
-        {
-            return GreatestCommonDivisor.EuclideanMethod(data);
+        [TestCase(3)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EuclideanMethodTest_OneArgumentException(params int[] values){
+            Assert.AreEqual(GreatestCommonDivisor.EuclideanMethod(out _time, values), 0);
         }
         
         [Test]
@@ -46,34 +40,28 @@ namespace Task2.Tests
         [TestCase(3, 15, Result = 3)]
         [TestCase(3, 4, Result = 1)]
         [TestCase(0, 0, Result = 0)]
-        public int SteinMethodTest(int a, int b)
-        {
-            return GreatestCommonDivisor.SteinMethod(a, b);
-        }
-
-        [Test]
-        [TestCase(-3, 4)]
-        [TestCase(3, -5)]
-        [ExpectedException(typeof(ArgumentException))]
-        public void SteinMethodTest_Exception(int a, int b)
-        {
-            Assert.AreEqual(GreatestCommonDivisor.SteinMethod(a, b), 0);
-        }
-
-        [Test]
-        [TestCase(3, 15, 45, Result = 3)]
-        [TestCase(45, 15, 45, Result = 15)]
-        public int SteinMethodTest_3arg(int a, int b, int c)
-        {
-            return GreatestCommonDivisor.SteinMethod(a, b, c);
+        public int SteinMethodTest(int a, int b) {
+            return GreatestCommonDivisor.SteinMethod(out _time, a, b);
         }
         [Test]
         [TestCase(15, 45, 3, 15, Result = 3)]
         [TestCase(5, 45, 5, 1, 5, Result = 1)]
-        [TestCase(5, Result = 5)]
-        public int SteinMethodTest_Params(params int[] data)
-        {
-            return GreatestCommonDivisor.SteinMethod(data);
+        public int SteinMethodTest_Params(params int[] data){
+            return GreatestCommonDivisor.SteinMethod(out _time, data);
+        }
+        [Test]
+        [TestCase(-3, 4)]
+        [TestCase(3, -5)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SteinMethodTest_NegativeArgumentException(int a, int b) {
+            Assert.AreEqual(GreatestCommonDivisor.SteinMethod(out _time,a, b), 0);
+        }
+
+        [Test]
+        [TestCase(3)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SteinMethodTest_OneArgumentException(params int[] values){
+            Assert.AreEqual(GreatestCommonDivisor.SteinMethod(out _time, values), 0);
         }
 
     }
--- Task2/GreatestCommonDivisor.cs
+++ Task2/GreatestCommonDivisor.cs
@@ -1,23 +1,49 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Task2 {
     public static class GreatestCommonDivisor {
+        public static int EuclideanMethod(out TimeSpan time,int a, int b) {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int gcd = EuclideanMethod(a, b);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed;
+            return gcd;
+        }
         public static int EuclideanMethod(int a, int b) {
             if (a == 0 && b == 0)
                 throw new ArgumentException("");
             return b > a ? GcdEuclide(b, a) : GcdEuclide(a, b);
         }
 
-        public static int EuclideanMethod(int a, int b, int c) {
-            return EuclideanMethod(a, EuclideanMethod(b, c));
+        public static int EuclideanMethod(out TimeSpan time, params int[] values) {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int gcd = EuclideanMethod(values);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed;
+            return gcd;
         }
 
         public static int EuclideanMethod(params int[] values) {
+            if (values.Length < 2)
+                throw new ArgumentException("Arguments must be at least 2");
             int result = values[0];
-            for (int i = 1; i < values.Length; i++) {
+            for (int i = 1; i < values.Length; i++){
                 result = EuclideanMethod(values[i], result);
             }
             return result;
+
+        }
+
+        public static int SteinMethod(out TimeSpan time, int a, int b) {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int gcd = SteinMethod(a, b);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed;
+            return gcd;
         }
 
         public static int SteinMethod(int a, int b) {
@@ -25,25 +51,29 @@ namespace Task2 {
                 throw new ArgumentException("Arguments must be nonegative.");
             return GcdStein(a, b);
         }
-        public static int SteinMethod(int a, int b, int c) {
-            return SteinMethod(a, SteinMethod(b, c));
+
+        public static int SteinMethod(out TimeSpan time, params int[] values) {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int gcd = SteinMethod(values);
+            stopWatch.Stop();
+            time = stopWatch.Elapsed;
+            return gcd;
         }
-        public static int SteinMethod(params int[] values)
-        {
-            int result = values[0];
-            for (int i = 1; i < values.Length; i++)
-            {
-                result = SteinMethod(values[i], result);
+
+        public static int SteinMethod(params int[] values) {
+            if (values.Length < 2)
+                throw new ArgumentException("Arguments must be at least 2");
+            int gcd = values[0];
+            for (int i = 1; i < values.Length; i++){
+                gcd = SteinMethod(values[i], gcd);
             }
-            return result;
+            return gcd;
         }
 
 
         private static int GcdEuclide(int a, int b) {
-            if (b != 0) {
-                return GcdEuclide(b, a%b);
-            }
-            return Math.Abs(a);
+            return b != 0 ? GcdEuclide(b, a%b) : Math.Abs(a);
         }
 
         private static int GcdStein(int a, int b) {
