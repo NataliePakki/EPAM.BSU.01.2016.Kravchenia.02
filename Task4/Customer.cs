2327e3ea85f119a4a15e98894f88299be3d50452
Natalie Pakki <fecitinboxru@gmail.com>
2016-03-17 15:14:37 +0300

    rechange ToString()

--- Task4/Customer.cs
+++ Task4/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Task4 {
     public class Customer:IFormattable {
@@ -12,28 +13,37 @@ namespace Task4 {
             Recenue = rec;
         }
         public override string ToString() {
-            return ToString("g",null);
+            return ToString("g",new  CultureInfo("en-US"));
         }
         
         public string ToString(string format, IFormatProvider formatProvider) {
             if (String.IsNullOrEmpty(format)) format = "g";
-            var fmt = formatProvider?.GetFormat(GetType()) as ICustomFormatter;
-            if (fmt != null) { return fmt.Format(null, this, formatProvider); }
-            switch (format.ToLower()){
-                 case "g":
-                 case "name phone recenue":
-                     return $"Customer record: {Name}, {ContactPhone}, {Recenue}";
-                 case "name":
-                     return $"Customer record: {Name}";
-                 case "name phone":
-                     return $"Customer record: {Name}, {ContactPhone}";
-                 case "phone":
-                     return $"Customer record: {ContactPhone}";
-                 case "recenue":
-                     return $"Customer record: {Recenue}";
-                 default:
-                     throw new FormatException($"The '{format}' format string is not supported.");
-             }
+            if(formatProvider == null) {
+                formatProvider = new CultureInfo("en-US");
+            }
+           
+            switch (format.ToLowerInvariant()){
+                case "nrp":
+                if(formatProvider is CustomerFormatProvider)
+                    return string.Format(formatProvider, "{0:N}, {1:R}, {2:P}", Name, Recenue, ContactPhone);
+                return string.Format(formatProvider, "{0}, {1:C}, {2}", Name, Recenue, ContactPhone);
+                case "g":
+                case "npr":
+                if(formatProvider is CustomerFormatProvider)
+                    return string.Format(formatProvider, "{0:N}, {1:P}, {2:R} ", Name, ContactPhone, Recenue);
+                return string.Format(formatProvider, "{0}, {1}, {2:C}", Name, ContactPhone, Recenue);
+                case "nr":
+                if(formatProvider is CustomerFormatProvider)
+                    return string.Format(formatProvider, "{0:N}, {1:R}", Name, Recenue);
+                return string.Format(formatProvider, "{0}, {1:C}", Name, Recenue);
+                case "r":
+                if(formatProvider is CustomerFormatProvider)
+                    return string.Format(formatProvider, "{0:R}", Recenue);
+                return string.Format(formatProvider, "{0:C}", Recenue);
+                default:
+                throw new FormatException($"The {format} format string is not supported.");
+
+            }
         }
         public string ToString(IFormatProvider formatProvider) {
             return ToString(null, formatProvider);
