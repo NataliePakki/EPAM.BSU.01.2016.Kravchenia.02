2adcde5337ad26db4c76cf503e22fbbccf1ae0e7
Natalie Pakki <fecitinboxru@gmail.com>
2016-03-17 15:13:43 +0300

    add Tests

--- Task4.Tests/CustomerTests.cs
+++ Task4.Tests/CustomerTests.cs
@@ -5,31 +5,32 @@ using NUnit.Framework;
 namespace Task4.Tests {
     public class CustomerTests {
         private readonly Customer cust = new Customer("John", "+375-17-4341283", 10000);
-       
-
+      
         [Test]
-        [TestCase(Result = "Customer record: John, +375-17-4341283, 10000")]
+        [TestCase(Result = "John, +375-17-4341283, $10,000.00")]
         public string CustomerToStringTest() {
             return cust.ToString();
         }
 
         [Test]
-        [TestCase(Result = "Customer record: +375-17-4341283, John, 10000")]
+        [TestCase(Result = "Name: John, Contact phone: +375-17-4341283, Recenue: $10,000.00 ")]
         public string CustomerToStringTest_CustomerFormatProvider() {
-            return cust.ToString(new CustomerFormatProvider());
-        }
-
-
-        [Test]
-        [TestCase(Result = "Customer record: John, +375-17-4341283")]
-        public string CustomerToStringTest_formatNamePhone() {
-            return cust.ToString("name phone");
+            return cust.ToString("npr",new CustomerFormatProvider());
         }
         [Test]
-        [ExpectedException(typeof(FormatException))]
-        [TestCase(Result = "Customer record: John, 10000")]
-        public string CustomerToStringTest_Exception() {
-            return cust.ToString("name recenue");
+        [TestCase(Result = "10.000,00 €")]
+        public string CustomerToStringTest_DeFormatProvider() {
+            return cust.ToString("r",new CultureInfo("de-DE"));
+        }
+        [Test]
+        [TestCase(Result = "$10,000.00")]
+        public string CustomerToStringTest_NullFormatProvider() {
+            return cust.ToString("r", null);
+        }
+        [Test]
+        [TestCase(Result = "John, +375-17-4341283, $10,000.00")]
+        public string CustomerToStringTest_GFormat() {
+            return cust.ToString("g", null);
         }
     }
 }
